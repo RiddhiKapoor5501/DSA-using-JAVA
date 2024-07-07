3891827fa37c453217f17681ae60970eaac11b3b
RIDDHI KAPOOR <100359128+RiddhiKapoor5501@users.noreply.github.com>
2024-07-07 20:51:50 +0530

    Add files via upload

---
+++ Basics/JavaBasics.java
@@ -0,0 +1,7 @@
+public class JavaBasics{
+    public static void main(String args[]) {
+        System.out.println("Hello Riddhi");
+        System.out.println("Hello Riddhi");
+
+    }
+}
---
+++ Basics/conditions.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class conditions{
+    public static void main(String args[]) {
+       Scanner sc = new Scanner (System. in);
+       int x = sc.nextInt();
+
+       if(x > 0) {
+          System.out.println(positive);
+       } else {
+          System.out.println(negative);
+       }
+       
+       
+       
+       
+    }
+       
+       
+}
